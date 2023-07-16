c1048a44769917d8bd1fb53d80dda5262eb864f9
Александр Симич <a.simich@neurus.ru>
2023-07-16 13:56:02 +0300

    hotfix2

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char* argv[])
 {
-    if (argc == 1) {
-        std::string libmodel = argv[0];
+    if (argc == 2) {
+        std::string libmodel = argv[1];
         std::cout << "Start" << std::endl;
         auto model = tvm::runtime::Module::LoadFromFile(libmodel);
         std::cout << "Loaded" << std::endl;
